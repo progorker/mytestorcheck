e7d641d97c32d5675945216b806aa0964aed1a0e
[ progorker ] Zino's Petrel <zinospetrel@sdf.org>
2026-06-11 18:02:14 +0700

    Add files via upload

--- myTestorCheck/tst/procedure_test_strings.sql
+++ myTestorCheck/tst/procedure_test_strings.sql
@@ -2,7 +2,7 @@
  * Copyright (c) 2026 Dinh Thoai Tran <zinospetrel@sdf.org>
  * All rights reserved.
  *
- * + Source URL: https://github.com/progorker/mytestorcheck/
+ * + Source URL: https://github.com/progorker/pgk_mytestorcheck/
  *
  * + License: GPL-2.0
  */
